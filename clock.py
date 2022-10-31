16f38e03e755d3e4eda417e8348400b5c5597f3e
samanthaanders <112581381+samanthaanders@users.noreply.github.com>
2022-10-31 13:39:47 -0600

    updates

--- clock.py
+++ clock.py
@@ -1,6 +1,8 @@
-from datetime import date 
-from datetime import datetime
+#from datetime import date 
+import datetime
+from math import trunc
 import threading
+import tkinter as tk
 
 periods = ["Warning", "Period 1","Break 1","Period 2","Lunch","Period 3","Break 2","Period 4"]
 
@@ -21,4 +23,59 @@ def set_interval(func, sec):
     t.start()
     return t
 
-x = set_interval(def())
+
+def set_interval(): 
+    date_now = datetime.datetime(create_date_time)
+    weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    weekday = weekdays[date_now]
+    hour = date_now.stfrtime("%H") 
+    ampm = "AM"
+    if hour > 11:
+        ampm = "PM"
+    if hour > 12:
+        hour = hour - 12
+    minute = date_now.stfrtime("%M")
+    if minute < 10:
+        minute = "0" + minute
+
+# section where colours & formatting are set 
+
+    if (date_now == 0) or (date_now == 6):
+        # change colour
+        x = 1 # placeholder
+    else:
+        if date_now == 3:
+            start_times = ["8:30","8:35","9:46","9:53","11:04","11:48","12:59","13:06","14:17"]
+        else:
+            start_times = ["8:30","8:35","10:02","10:09","11:36","12:15","13:43","13:50","15:17"]
+        for i in periods:
+            start_time = create_date_time(start_times[i])
+            end_time = create_date_time(start_times[i + 1])
+            if (date_now > start_time) and (date_now < end_time):
+                difference = end_time - date_now
+                period = periods[i]
+                break
+            else:
+                difference = ""
+                period = ""
+    
+    hours = trunc(difference % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    minutes = trunc((difference % (1000 * 60 * 60)) / (1000 * 60))
+    seconds = trunc((difference % (1000 * 60)) / 1000)
+
+    if seconds < 10:
+        seconds = "0" + seconds
+    if hours > 0:
+        if minutes < 10:
+            minutes = "0" + minutes
+        # style/color
+    else:
+        # style/color
+        if minutes < 2:
+            # yellow color
+            x = 0 # placeholder
+        if (minutes == 0) and (seconds == 0):
+            # white color
+            x = 0 # placeholder
+
+
